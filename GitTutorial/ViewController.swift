5d19a255b97f76eac462e4ad1c4b9575fa3dcbfd
Sercan Orhangazi <sercanorhangazi@gmail.com>
2018-08-29 08:11:10 +0300

    Reverted

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("Hoşgeldiniz.")
-        print("Sercan Orhangazi")
-        
+        // Do any additional setup after loading the view, typically from a nib.
     }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
 
 }
 
